df6ba01d0fecc124cac8d481031183dd24f1b188
Dayo Akinkuowo <me@sprypixels.com>
2020-04-26 04:33:49 +0100

    Splash screen, app icon added

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import Navigation from './src/navigation';
 
 const App = () => {
+  SplashScreen.hide();
   return (
     <NavigationContainer>
       <Navigation />
